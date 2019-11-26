366adfb81a6a75b15085c1deb3eab0aee9b468f6
wxc <1165124074@qq.com>
2019-11-26 19:24:34 +0800

    first commit

---
+++ src/ALGO/ALGO_1.java
@@ -0,0 +1,8 @@
+package ALGO;
+//算法训练 区间k大数查询
+//http://lx.lanqiao.cn/problem.page?gpid=T11
+public class ALGO_1 {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
